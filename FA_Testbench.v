a08582c3d2f33a918d604884f4493919fbfba135
Shraddha1129 <kshraddha254@gmail.com>
2025-03-01 12:29:50 +0530

    Add files via upload

---
+++ FA_Testbench.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+module FA_Testbench_v;
+// Inputs
+	 wire A;
+	 wire B;
+	 wire Cin;
+
+	// Outputs
+	wire Sum;
+	wire Carry;
+
+	// Instantiate the Unit Under Test (UUT)
+	FA uut (
+		.A(A), 
+		.B(B), 
+		.Cin(Cin), 
+		.Sum(Sum), 
+		.Carry(Carry)
+	);
+	initial begin
+		A = 0; B = 0; Cin = 0; #10; //Test cases for different inputs
+		A = 0; B = 0; Cin = 1; #10;
+   	A = 0; B = 1; Cin = 0; #10;
+		A = 0; B = 1; Cin = 1; #10;
+		A = 1; B = 0; Cin = 0; #10;
+		A = 1; B = 0; Cin = 1; #10;
+		A = 1; B = 1; Cin = 0; #10;
+		A = 1; B = 1; Cin = 1; #10;
+		$display("Test completed ");
+		$finish;
+	end      
+endmodule
+
